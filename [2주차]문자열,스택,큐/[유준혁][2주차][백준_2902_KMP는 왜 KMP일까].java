d5d5ba0b696f487ede96d7736e9617f8a43f5831
J511Y <47891005+J511Y@users.noreply.github.com>
2019-09-04 02:32:28 +0900

    [유준혁][2주차][백준_2902_KMP는 왜 KMP일까]

---
+++ [2주차]문자열,스택,큐/[유준혁][2주차][백준_2902_KMP는 왜 KMP일까].java
@@ -0,0 +1,8 @@
+package workspace;
+import java.util.*;
+public class baekjoon_2902 {
+	public static void main(String[] args) {
+		String [] arr = new Scanner(System.in).next().split("-");
+		for(String s : arr)System.out.print(s.charAt(0));
+	}
+}
