760496bdfe1ce1ec582cc0089749d36c8f260432
J511Y <47891005+J511Y@users.noreply.github.com>
2019-09-04 18:09:31 +0900

    [유준혁][2주차][백준_1120_문자열]

---
+++ [2주차]문자열,스택,큐/[유준혁][2주차][백준_1120_문자열].java
@@ -0,0 +1,26 @@
+package workspace;
+import java.util.*;
+public class baekjoon_1120 {
+	public static int count(String a , String b) {
+		int cnt=0;
+		for(int i=0; i<a.length(); i++) {
+			if(a.charAt(i) != b.charAt(i)) cnt++;
+		}
+		return cnt;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a=sc.next(), b=sc.next();
+		int aLen=a.length(), bLen=b.length(), cnt=0;
+		if(aLen==bLen) {
+			System.out.println(count(a,b));
+		}else {
+			cnt=count(a , b.substring(bLen-aLen));
+			for(int i=0; i<bLen-aLen; i++) {
+				int func = count(a , b.substring(i,aLen+i));
+				if (cnt > func) cnt=func;
+			}
+			System.out.println(cnt);
+		}
+	}
+}
