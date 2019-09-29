7a6c769295cbc72b5ba23e99789a1dfb5466491b
J511Y <47891005+J511Y@users.noreply.github.com>
2019-09-29 21:26:24 +0900

    7568_덩치 수정

--- [5주차]브루트포스/[유준혁][5주차][백준_7568_덩치].java
+++ [5주차]브루트포스/[유준혁][5주차][백준_7568_덩치].java
@@ -1,10 +1,20 @@
-package baekjoon_brute_force;
-
-public class baekjoon_7568 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.*;
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		int [][] arr = new int [cnt][2];
+		for(int i=0; i<cnt; i++){
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+		for(int i=0; i<cnt; i++){
+			int rank = 1;
+			for(int j=0; j<cnt; j++){
+				if (i == j) continue;
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) rank++;
+			}
+			System.out.print(rank+" ");
+		}
 	}
-
 }
