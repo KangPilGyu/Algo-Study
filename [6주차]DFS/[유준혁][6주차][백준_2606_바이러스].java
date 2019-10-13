e2f21c2a0ae9f5f8d1e0767448b98bd9753e9d15
J511Y <47891005+J511Y@users.noreply.github.com>
2019-10-13 21:18:14 +0900

    2606_수정

--- [6주차]DFS/[유준혁][6주차][백준_2606_바이러스].java
+++ [6주차]DFS/[유준혁][6주차][백준_2606_바이러스].java
@@ -2,30 +2,30 @@ package baekjoon_DFS_BFS;
 import java.util.*;
 public class baekjoon_2606 {
 	static int [][] arr;
-	static Set<Integer> set = new HashSet<>();
+	static boolean [] visit;
 	static void func(int idx) {
-		set.add(idx);
-		for(int i=0; i<arr.length; i++)
-			if(arr[i][0] == idx && !set.contains(arr[i][1])) {
-//				System.out.println("현재 i ->"+i+", 현재 idx ->"+idx+", 새로 불러들인 idx ->"+arr[i][1]);
+		visit[idx] = true;
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i][0] == idx && !visit[arr[i][1]])
 				func(arr[i][1]);
-			}
+			if(arr[i][1] == idx && !visit[arr[i][0]])
+				func(arr[i][0]);
+		}
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		sc.nextInt();
+		int n = sc.nextInt();
 		int m = sc.nextInt();
-		arr = new int [m+1][2];
+		arr = new int [m][2];
+		visit = new boolean [n+1];
 		for(int i=0; i<m; i++) {
 			arr[i][0] = sc.nextInt();
 			arr[i][1] = sc.nextInt();
 		}
-		for(int [] ar : arr) {
-			for(int i : ar) System.out.print(i+" ");
-			System.out.println();
-		}
 		func(1);
-		System.out.println(set.size() - 1);
+		int sum = 0;
+		for(int i=0; i<n+1; i++) if(visit[i]) sum++;
+		System.out.println(sum - 1);
 	}
 }
 /*
