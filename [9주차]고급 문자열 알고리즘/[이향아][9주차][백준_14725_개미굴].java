27e5a8b92522fbffd5b32f90f0cc0984596fe7d3
hyang <dlgiddk20@gmail.com>
2019-11-11 19:23:02 +0900

    [이향아][9주차][백준_14725_개미굴]

---
+++ [9주차]고급 문자열 알고리즘/[이향아][9주차][백준_14725_개미굴].java
@@ -0,0 +1,29 @@
+package week9;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class °³¹Ì±¼_14725 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		
+		for (int i = 0; i < n; i++) {
+			int k = sc.nextInt();
+			String[] antsHill = new String[k];
+			
+			for (int j = 0; j < k; j++) {
+				antsHill[j] = sc.nextLine();
+				if(antsHill[j+1].equals("")) {
+					
+				}
+			}
+					
+		}
+		
+	}
+
+}
