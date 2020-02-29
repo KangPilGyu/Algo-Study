96d826786e7620b05dc2e305f03bc143aedd0bdc
J511Y <47891005+J511Y@users.noreply.github.com>
2020-03-01 01:05:47 +0900

    [유준혁][24주차][백준_1547_공]

---
+++ [24주차]시뮬레이션/[유준혁][24주차][백준_1547_공].java
@@ -0,0 +1,18 @@
+package baekjoon_simulation;
+// https://www.acmicpc.net/problem/1547
+// 공
+// 문제 설명
+// --------
+// 야바위 맞추면 됨.
+import java.util.*;
+public class baekjoon_1547 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), ball = 1, a, b;
+		for(int i = 0; i < n; i++) {
+			a = sc.nextInt(); b = sc.nextInt();
+			ball = a == ball ? b : b == ball ? a : ball;
+		}
+		System.out.println(ball);
+	}
+}
