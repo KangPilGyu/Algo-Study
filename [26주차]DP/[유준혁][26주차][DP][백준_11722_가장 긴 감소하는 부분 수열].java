954406d38c57e6fc170cb710b016a20781a7a16f
J511Y <47891005+J511Y@users.noreply.github.com>
2020-04-11 21:11:56 +0900

    [유준혁][26주차][DP][백준_11722_가장 긴 감소하는 부분 수열]

---
+++ [26주차]DP/[유준혁][26주차][DP][백준_11722_가장 긴 감소하는 부분 수열].java
@@ -0,0 +1,21 @@
+package baekjoon_lis;
+//https://www.acmicpc.net/problem/11722
+//가장 긴 감소하는 부분 수열
+import java.util.*;
+public class baekjoon_11722 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int [n], dp = new int [n+1];
+		for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			dp[i] = 1;
+			for(int j = 0; j < i; j++) {
+				if(arr[i] < arr[j] && dp[i] < dp[j] + 1) {
+	                dp[i] = dp[j] + 1;
+	            }
+			}
+		}
+		System.out.println(Arrays.stream(dp).max().getAsInt());
+	}
+}
