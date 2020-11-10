440305a8a5aeee394dec9eb776adecacb7375f3b
J511Y <47891005+J511Y@users.noreply.github.com>
2020-11-10 12:11:09 +0900

    유준혁 이분탐색 과제완료

---
+++ [2020년 10월 시작]/[6주차]이분탐색/[유준혁][백준_1072_게임].java
@@ -0,0 +1,22 @@
+package baekjoon_binary_search;
+// https://www.acmicpc.net/problem/1072
+// 게임
+
+import java.util.*;
+class baekjoon_1072{
+	public static void main(String[] ar){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), m = sc.nextInt(), r = (int)((double)m * 100 / n), left = 1, right = 1 << 30, rst = 1 << 30;
+		if(r < 99){
+			while(left <= right){
+				int mid = (left / 2) + (right / 2);
+				int R = (int)((double)(m + mid) * 100 / (n + mid));
+				if(r < R){
+					rst = Math.min(rst, mid);
+					right = mid - 1;
+				}else left = mid + 1;
+			}
+			System.out.print(rst);
+		}else System.out.print(-1);
+	}
+}
